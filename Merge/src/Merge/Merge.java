573fbd7db3c9054171a2508d1358c8e40ab51d9b
kowsalya30g <78354123+kowsalya30g@users.noreply.github.com>
2021-02-08 13:08:03 +0530

    Update Merge.java

--- Merge/src/Merge/Merge.java
+++ Merge/src/Merge/Merge.java
@@ -6,6 +6,9 @@ public class Merge {
 		System.out.println("Hello merge conflict");
 		System.out.println("WELCOME TO POSSIBLE");
 		System.out.println("hiii");
+		int a = 5;
+		int b = 5;
+		System.out.println("a+b");
 	}
 
 }
